0fe7e0ca44409397a3b3e851bbcef58c75ffcb4b
Pldevweb <101077113+Pldevweb@users.noreply.github.com>
2023-03-30 17:49:44 -0400

    Add files via upload

---
+++ Projet-integrateur/views/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once ('../public/assets/header.html');
+
+echo ('<h1>EPLD COMPUTERS</h1>');
+
+$id = $_GET['id'];
+
+echo "ID: " . $id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require_once ('../public/assets/footer.html');
+?>
